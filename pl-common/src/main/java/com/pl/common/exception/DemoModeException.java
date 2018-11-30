70f0203999bf2f6568c575108e6001ff3f411cd2
JiangPeng <121670142@qq.com>
2018-11-30 17:58:19 +0800

    首次提交

---
+++ pl-common/src/main/java/com/pl/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package com.pl.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author ruoyi
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
